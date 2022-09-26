b4bfb1b3758fa4a6926a8c8a88308c108c99e877
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-09-26 19:00:30 +0300

    task010 added

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int getSecondDigitalFromNumber(int number)
+{
+    number = (number%100)/10;
+    int result = number;
+    return(result);
+}
+Console.WriteLine("Введите число:");
+int userNumber = 0;
+userNumber = Convert.ToInt32(Console.ReadLine());
+int delNumber = getSecondDigitalFromNumber(userNumber);
+Console.WriteLine($"Вторая цифра {delNumber}");
