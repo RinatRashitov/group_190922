22e73f79610d3b3475085d7772141f2a79726343
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-09-28 21:52:10 +0300

    task21 midyfied 3d

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,5 +1,7 @@
-﻿/*21: Напишите программу, которая принимает на вход координаты двух точек и находит
- расстояние между ними в 2D пространстве. A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21*/
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 int getCoorditateFromUser(string userInformation)
 {
@@ -18,16 +20,18 @@ int getCoorditateFromUser(string userInformation)
     return (result);
 }
 
-double getDistance(int AX, int AY, int BX, int BY)
+double getDistance(int AX, int AY, int AZ, int BX, int BY, int BZ)
 {
-    double result=0;
-    result=Math.Sqrt(Math.Pow((AX-BX),2)+Math.Pow((AY-BY),2));
-    return(result);
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((AX - BX), 2) + Math.Pow((AY - BY),2) + Math.Pow((AZ - BZ), 2));
+    return (result);
 }
 int coordinateAX = getCoorditateFromUser("Введите координату х для точки A ");
 int coordinateAY = getCoorditateFromUser("Введите координату y для точки A ");
+int coordinateAZ = getCoorditateFromUser("Введите координату z для точки A ");
 int coordinateBX = getCoorditateFromUser("Введите координату х для точки B ");
 int coordinateBY = getCoorditateFromUser("Введите координату y для точки B ");
+int coordinateBZ = getCoorditateFromUser("Введите координату z для точки B ");
 
-double distance = Math.Round(getDistance(coordinateAX, coordinateAY, coordinateBX, coordinateBY),2);
-Console.WriteLine($"Расстояние между точками A и B с координатами ({coordinateAX },{coordinateAY}), ({coordinateAY},{coordinateBY}) составляет {distance}");
+double distance = Math.Round(getDistance(coordinateAX, coordinateAY, coordinateAZ, coordinateBX, coordinateBY, coordinateBZ), 2);
+Console.WriteLine($"Расстояние между точками A и B с координатами ({coordinateAX},{coordinateAY}), ({coordinateAY},{coordinateBY}) составляет {distance}");
