50e89801f399f17011b47c15bdc2a8bda4d602f2
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-09-28 20:04:59 +0300

    task24 added

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N. 5 -> 1, 4, 9, 16, 25. 2 -> 1,4*/
+
+Console.WriteLine("Введите число ");
+int userNumber = 0;
+string result="1";
+int i=0;
+userNumber = Convert.ToInt32(Console.ReadLine());
+// эта строка сделана для варианта задачи, когда и для отрицательных N нужно решение
+// в задаче просто не указано могу ли N быть отрицательными
+//userNumber = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+for(i=2;i<=userNumber;i++)
+{
+    result = result+", "+Math.Pow(i,2);
+}
+Console.WriteLine(result);
