5dbc7cc07a10b03b4aac5cfd0293e5ba69e29295
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-02 13:20:37 +0300

    Added task28

---
+++ Task28/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+int getFaсtorialNumber(int userNum)
+{
+    int result=1;
+    for(int j=1;j<=userNum;j++)
+    {
+        result = result*j;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int userNumber=Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"Факториал числа {userNumber} равен {getFaсtorialNumber(userNumber)}");
