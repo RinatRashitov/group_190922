d493dd4d5413cf04d26dc0dd16a6ddc049919db4
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-02 15:45:13 +0300

    Task29 added

---
+++ Task29/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 29: Напишите программу, в которой пользователь задает длину массива, элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]*/
+
+int[] createArray(int userNum)
+{
+    int[] userAr = new int[userNum];
+    return userAr;
+}
+
+void PrintArray(int[] usArray)
+{
+    Console.Write("[");
+    int index = usArray.Length;
+    for (int j = 0; j < index - 1; j++)
+    {
+        Console.Write(usArray[j]);
+        Console.Write(", ");
+    }
+    Console.Write(usArray[index - 1]);
+    Console.Write("]");
+}
+
+int[] FillDataArray(int[] usArray, int numFrom, int numTo)
+{
+    for (int j = 0; j < usArray.Length; j++)
+    {
+        usArray[j] = new Random().Next(numFrom, numTo);
+    }
+    return usArray;
+}
+
+Console.WriteLine("Введите число элементов в массиве: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента: ");
+int numFrom = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента: ");
+int numTo = Convert.ToInt32(Console.ReadLine());
+int[] UserArray = createArray(userNumber);
+//PrintArray(UserArray);
+Console.WriteLine("Полученный массив");
+PrintArray(FillDataArray(UserArray, numFrom, numTo+1));
