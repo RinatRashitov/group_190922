99c4eb58d55a9c82971222a40743188d0749a0db
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-02 15:40:40 +0300

    Task27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int calcSumDigital(int inNumber)
+{
+    int result = 0;
+    while (inNumber > 0)
+    {
+        result = result + inNumber % 10;
+        inNumber = inNumber / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел в числе {userNumber} в равна {calcSumDigital(userNumber)}");
