c351fca165dc9ef3193fd9413f4721aedf2dccff
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-12 20:44:19 +0300

    task44 added

---
+++ Task44/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8*/
+
+int getNextFibonacci (int firstNumber, int secondNumber)
+{
+    return firstNumber+secondNumber;
+}
+
+Console.WriteLine("Введите число N: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Последовательность чисел Фиббоначи для {userNumber} равно 0, 1");
+int numOne=0;
+int numTwo=1;
+int count=3;
+while(count<=userNumber)
+{
+    int numThree=getNextFibonacci(numOne, numTwo);
+    Console.Write($", {numThree}");
+    numOne=numTwo;
+    numTwo= numThree;
+    count=count+1;
+}
