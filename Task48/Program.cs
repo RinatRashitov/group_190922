d44dd2cc4bb8a5de28e007217d01e5555aa89fcc
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-13 20:34:48 +0300

    task48 added

---
+++ Task48/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5*/
+
+int getIntNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, Вы ввели: {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+int[,] create2DArray(int userNumX, int userNumY)
+{
+    int[,] userAr = new int[userNumX, userNumY];
+    return userAr;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor=ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void Print2DArray(int[,] usArray)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < usArray.GetLength(1); j++)
+    {
+        printColorData(j+"\t");
+    }
+    Console.WriteLine();
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        printColorData(j+"\t");
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            Console.Write($"{usArray[j, k]}" + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] Fill2DDataArray(int[,] usArray)
+{
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            usArray[j, k] = j+k;
+        }
+    }
+    return usArray;
+}
+
+int numX = getIntNumberFromUser("Введите значение по вертикали ");
+int numY = getIntNumberFromUser("Введите значение по горизонтали ");
+int[,] userArray = create2DArray(numX, numY);
+userArray = Fill2DDataArray(userArray);
+Print2DArray(userArray);
