c6c240b1a8ec53a6e1fd2451473e8c2a736bdf6a
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-13 20:54:35 +0300

    task49 added

---
+++ Task49/Program.cs
@@ -0,0 +1,93 @@
+﻿/*Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4*/
+
+int getIntNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, Вы ввели: {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+int[,] create2DArray(int userNumX, int userNumY)
+{
+    int[,] userAr = new int[userNumX, userNumY];
+    return userAr;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor=ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void Print2DArray(int[,] usArray)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < usArray.GetLength(1); j++)
+    {
+        printColorData(j+"\t");
+    }
+    Console.WriteLine();
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        printColorData(j+"\t");
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            Console.Write($"{usArray[j, k]}" + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] Fill2DDataArray(int[,] usArray, int numFrom, int numTo)
+{
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            usArray[j, k] = new Random().Next(numFrom, numTo);
+        }
+    }
+    return usArray;
+}
+
+int[,] replaceEvenSquareDataArray(int[,] usArray)
+{
+    for (int j = 0; j < usArray.GetLength(0); j=j+2)
+    {
+        for (int k = 0; k < usArray.GetLength(1); k=k+2)
+        {
+            usArray[j, k] = usArray[j, k]*usArray[j, k];
+        }
+    }
+    return usArray;
+}
+
+int numX = getIntNumberFromUser("Введите значение по вертикали ");
+int numY = getIntNumberFromUser("Введите значение по горизонтали ");
+int numFrom = getIntNumberFromUser("Введите минимальное значение элемента: ");
+int numTo = getIntNumberFromUser("Введите максимальное значение элемента: ");
+int[,] userArray = create2DArray(numX, numY);
+userArray = Fill2DDataArray(userArray, numFrom, numTo + 1);
+Console.WriteLine("Первоначальная матрица");
+Print2DArray(userArray);
+userArray = replaceEvenSquareDataArray(userArray);
+Console.WriteLine("Измененная матрица");
+Print2DArray(userArray);
