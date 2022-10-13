76b667cbcf22526b9cb65a88992226714ca06b06
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-13 23:25:31 +0300

    task52 added

---
+++ Task52/Program.cs
@@ -0,0 +1,92 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int getIntNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, Вы ввели: {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+int[,] create2DArray(int userNumX, int userNumY)
+{
+    int[,] userAr = new int[userNumX, userNumY];
+    return userAr;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void Print2DArray(int[,] usArray)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < usArray.GetLength(1); j++)
+    {
+        printColorData(j + "\t");
+    }
+    Console.WriteLine();
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        printColorData(j + "\t");
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            Console.Write($"{usArray[j, k]}" + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
+int[,] Fill2DDataArray(int[,] usArray, int numFrom, int numTo)
+{
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            usArray[j, k] = new Random().Next(numFrom, numTo);
+        }
+    }
+    return usArray;
+}
+string getArithmeticalMeanColumnFromArray(int[,] usArray)
+{
+    string result = "Среднее арифметическое каждого столбца: ";
+    for (int j = 0; j < usArray.GetLength(1); j++)
+    {
+        double sumMean = 0;
+        for (int k = 0; k < usArray.GetLength(0); k++)
+        {
+            sumMean = sumMean + usArray[k, j];
+        }
+        sumMean = Math.Round(sumMean / usArray.GetLength(0), 1);
+        result = result + sumMean + "; ";
+    }
+    return result;
+}
+
+int numX = getIntNumberFromUser("Введите значение по вертикали ");
+int numY = getIntNumberFromUser("Введите значение по горизонтали ");
+int numFrom = getIntNumberFromUser("Введите минимальное значение элемента: ");
+int numTo = getIntNumberFromUser("Введите максимальное значение элемента: ");
+int[,] userArray = create2DArray(numX, numY);
+userArray = Fill2DDataArray(userArray, numFrom, numTo + 1);
+Console.WriteLine("Матрица");
+Print2DArray(userArray);
+Console.WriteLine($"{getArithmeticalMeanColumnFromArray(userArray)}");
+
