6bd8dd5fe6433efd209be4ac1a3ee1898e465f20
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-15 22:04:20 +0300

    task54 added

---
+++ Task54/Program.cs
@@ -0,0 +1,101 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int getIntNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, Вы ввели: {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+int[,] create2DArray(int userNumX, int userNumY)
+{
+    int[,] userAr = new int[userNumX, userNumY];
+    return userAr;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void Print2DArray(int[,] usArray)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < usArray.GetLength(1); j++)
+    {
+        printColorData(j + "\t");
+    }
+    Console.WriteLine();
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        printColorData(j + "\t");
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            Console.Write($"{usArray[j, k]}" + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
+int[,] Fill2DDataArray(int[,] usArray, int numFrom, int numTo)
+{
+    for (int j = 0; j < usArray.GetLength(0); j++)
+    {
+        for (int k = 0; k < usArray.GetLength(1); k++)
+        {
+            usArray[j, k] = new Random().Next(numFrom, numTo);
+        }
+    }
+    return usArray;
+}
+int[,] sortMinToMaxArray(int[,] usArray)
+{
+    for (int i = 0; i < usArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < usArray.GetLength(1); j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < usArray.GetLength(1); k++)
+            {
+                if (usArray[i, maxPosition] < usArray[i, k]) maxPosition = k;
+            }
+            int tmp = usArray[i, maxPosition];
+            usArray[i, maxPosition] = usArray[i, j];
+            usArray[i, j] = tmp;
+        }
+    }
+    return usArray;
+}
+
+int numX = getIntNumberFromUser("Введите значение по вертикали ");
+int numY = getIntNumberFromUser("Введите значение по горизонтали ");
+int numFrom = getIntNumberFromUser("Введите минимальное значение элемента: ");
+int numTo = getIntNumberFromUser("Введите максимальное значение элемента: ");
+int[,] userArray = create2DArray(numX, numY);
+userArray = Fill2DDataArray(userArray, numFrom, numTo + 1);
+Console.WriteLine("Исходный массив");
+Print2DArray(userArray);
+userArray = sortMinToMaxArray(userArray);
+Console.WriteLine("Отсортированный массив");
+Print2DArray(userArray);
+
+
