419fd6ee18c140d68049c7a503f8274bc952bdd6
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-17 20:56:29 +0300

    task63 added

---
+++ Task63/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"*/
+
+int getIntNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, Вы ввели: {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+void printFromOneToN(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    else
+    {
+        printFromOneToN(N - 1);
+        Console.Write($"{N}, ");
+    }
+}
+
+int numN = getIntNumberFromUser("Введите значение N ");
+Console.WriteLine("");
+printFromOneToN(numN);
