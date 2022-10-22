b2b897b67f4c08bcf8b6ef23d981d6f0bf3a5465
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-10-22 15:19:51 +0300

    task66 added

---
+++ Task66/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая сумму натуральных числе в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30*/
+int getIntNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Введите целое число, Вы ввели: {userLine}");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+int getSummFromMToN(int M, int N)
+{
+    int result=0;
+    while (N >= M)
+    {
+        result=result+N;
+        N=N-1;
+    }
+    return result;
+}
+
+int numM = getIntNumberFromUser("Введите значение M ");
+int numN = getIntNumberFromUser("Введите значение N ");
+Console.WriteLine("");
+Console.WriteLine($"Сумма чисел от {numM} до {numN} равна {getSummFromMToN(numM, numN)}");
